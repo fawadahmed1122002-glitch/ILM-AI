d153afa5ec8ae03fd2914d3c0659cbe583a69aee
Fawadahmed12309 <fawad123009@gmail.com>
2026-07-18 16:35:41 +0500

    added new frontend link

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app.add_middleware(SlowAPIMiddleware)
 # CORS — allow frontend dev server
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://prepxmentor-frontend-production.up.railway.app"],
+    allow_origins=["http://localhost:3000","https://prepxmentor.up.railway.app", "https://prepxmentor-frontend-production.up.railway.app"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["*"],
